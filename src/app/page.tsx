5ebb735d7d8b33c980af0c8465e7d5cdb5abba8d
izaias.junior <izaias.junior@grancursosonline.com.br>
2023-08-01 19:30:01 -0300

    feat: mais ajustes finos na tela de login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Login from '@/components/Login/Login'
 import dynamic from 'next/dynamic';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from './theme';
+import { CssBaseline } from '@mui/material';
+
 
 const DynamicLoginComponent = dynamic(() => import('@/components/Login/Login'), {
+  ssr: false, // Indica ao Next.js para não renderizar este componente no lado do servidor (server-side)
+});
+
+const DynamicRegisterComponent = dynamic(() => import('@/components/Cadastro/Cadastro'), {
   ssr: false // Indica ao Next.js para não renderizar este componente no lado do servidor (server-side)
 });
 
+
 export default function Home() {
+  
   return (
-    <>
-      <DynamicLoginComponent />
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <DynamicLoginComponent/>
+    </ThemeProvider>
   )
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,3 +1,4 @@
+'use client'
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import {
     Box,
@@ -13,47 +14,9 @@ import {
 } from '@mui/material';
 
 export default function Login() {
+    
     return (
-        <Grid container sx={{
-            "@media (max-width: 600px)": {
-                // centraliza todos os itens em telas pequenas
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                // retira a theme_img.png
-                "& > :nth-child(2)": {
-                    display: 'none'
-                },
-                // ajusta o tamanho do container
-                "& > :nth-child(1)": {
-                    width: '100%',
-                    height: '100vh'
-                }
-            },
-            // ajusta para dispositivos ente 360 a 400px
-            "@media (min-width: 360px) and (max-width: 400px)": {
-                // ajusta o tamanho do container
-                "& > :nth-child(1)": {
-                    height: '80vh',
-                    width: '100%'
-                },
-                // ajusta o tamanho da imagem logo.svg
-                "& > :nth-child(1) > :nth-child(1)": {
-                    height: '5rem',
-                    width: '15rem'
-                },
-                // ajusta o tamanho dos inputs
-                "& > :nth-child(1) > :nth-child(3)": {
-                    width: '100%',
-                    maxWidth: '20rem',
-                },
-                // ajusta o tamanho dos botões
-                "& > :nth-child(1) > :nth-child(4)": {
-                    width: '30rem',
-                    maxWidth: '20rem',
-                }
-            }
-        }}>
+        <Grid container>
             <Grid item xs={12} sm={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', gap: '1rem', padding: "0", height: '80vh' }}>
                 {/* grid centralizado para a logo centralizado para todos os tipos de telas */}
                 <Grid
@@ -70,25 +33,23 @@ export default function Login() {
                         }
                     }
                 >
-                    <Box
-                        component="img"
-                        src="/logo.svg"
-                        sx={
-                            { 
-                                height: '100%', 
-                                width: '100%',
-                            }
-                        }
-                    ></Box>
+                    <Grid container rowSpacing={3} >
+                        <Grid item xs={12} sm={6}>
+                            <Box
+                                component="img"
+                                src="/logo.svg"
+                                alt="logo"
+                            ></Box>
+                        </Grid>
+                    </Grid>
+                    
                 </Grid>
-                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-                    <Typography variant="h1" sx={{ fontWeight: 'bold', fontSize: '3rem', "@media (min-width: 360px) and (max-width: 400px)": { fontSize: '1.5rem' } }}>
-                        Bem-vindo ao Judici A.i.
-                    </Typography>
-                    <Typography variant='subtitle1' sx={{ fontSize: '1rem', "@media (min-width: 360px) and (max-width: 400px)": { width: "17rem" } }}>
-                        Um processo para automatizar seus processos.
-                    </Typography>
-                </Box>
+                <Typography variant='h1' sx={{ fontSize: '2.5rem', fontWeight: 'bold', color: '#1E293B', paddingTop: "5rem" }}>
+                    Bem-vindo(a) de volta!
+                </Typography>
+                <Typography variant='subtitle1' sx={{ fontSize: '1.5rem', color: '#1E293B' }}>
+                    Faça login para continuar
+                </Typography>
                
                 <Box sx={
                     
@@ -113,7 +74,7 @@ export default function Login() {
                     <FormControl variant="outlined" fullWidth>
                         <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
                         <OutlinedInput
-                            
+                            placeholder="Digite sua senha"
                             id="outlined-adornment-password"
                             type='password'
                             endAdornment={
@@ -130,7 +91,7 @@ export default function Login() {
                         />
                     </FormControl>
                     
-                <Grid container sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: '0.5rem' }}>
+                <Grid container rowSpacing={1} sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: '0.5rem' }}>
                     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <Typography variant='subtitle1' sx={{ fontSize: '1rem' }}>
                             Não tem uma conta?
@@ -151,29 +112,32 @@ export default function Login() {
                     
                 </Grid>
                 
-                <Button variant="contained" sx={
-                    { 
-                        backgroundColor: '#7C3AED', 
-                        fontWeight: 'bold', 
-                        fontSize: '1rem', 
-                        display: 'flex', 
-                        justifyContent: 'center', 
-                        alignItems: 'center', 
-                        width: '100%', 
-                        maxWidth: '30rem', 
-                        height: '4rem', 
-                        ":hover": { backgroundColor: '#600DEE' },
-                        "@media (min-width: 360px) and (max-width: 400px)": {
-                            width: '30rem',
-                            maxWidth: '18.5rem',
-                        }
-                    }}>
-                    Entrar
-                </Button>
+                <Grid container >
+                    <Grid item xs={12} sm={6}>
+                        <Button
+                            variant="contained"
+                            fullWidth
+                            sx={{ backgroundColor: '#7C3AED', color: '#fff', fontWeight: 'bold', fontSize: '1rem', padding: '1rem 0' }}
+                        >
+                            Entrar
+                        </Button>
+                    </Grid>
+                </Grid>
                 </Box>
             </Grid>
-            {/* Aqui adicionamos a propriedade display: none para esconder a imagem em telas pequenas (xs) */}
-            <Grid item xs={false} sm={6} sx={{ backgroundImage: 'url(/theme_img.png)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '100vh', zIndex: 1 }}></Grid>
+            <Grid item xs={12} sm={6}>
+                <Box
+                    component="img"
+                    src="/theme_img.png"
+                    alt="theme_img"
+                    sx={{
+                        width: '100%',
+                        height: '100vh',
+                        objectFit: 'cover',
+                        display: { xs: 'none', sm: 'block' }, // Esconde a imagem em telas pequenas (xs)
+                    }}
+                />
+            </Grid>
         </Grid>
     );
 }
