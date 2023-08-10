e0c305911cfa02cd43e46cafb4a3566b6fc29406
izaias.junior <izaias.junior@grancursosonline.com.br>
2023-08-09 21:23:27 -0300

    feat: inicia redux

--- src/components/Cadastro/Cadastro.tsx
+++ src/components/Cadastro/Cadastro.tsx
@@ -7,14 +7,52 @@ import {
   IconButton,
   InputLabel,
   OutlinedInput,
+  Theme,
   Typography,
+  Snackbar,
   useMediaQuery,
+  Alert,
+  colors,
+  AlertTitle,
 } from "@mui/material";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import React, { useState } from "react";
 
 
 const SignupPage = () => {
-  const isSmOrUp = useMediaQuery((theme) => theme.breakpoints.up("sm"));
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState({
+    nome: "",
+    sobrenome: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPass: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPass) {
+      setOpen(true);
+      setMessage("As senhas não coincidem");
+      return;
+    } else {
+      setOpen(false);
+    }
+
+    console.log(
+      formData
+    );
+  };
+
+  const isSmOrUp = useMediaQuery((theme: Theme) => theme.breakpoints.up("sm"));
+
   const renderBackButton = () => {
     if (isSmOrUp) return null;
     return (
@@ -30,7 +68,8 @@ const SignupPage = () => {
         <ArrowBackIcon />
       </IconButton>
     );
-  }
+  };
+
   return (
     <Grid container>
       {isSmOrUp && (
@@ -48,6 +87,7 @@ const SignupPage = () => {
       <Grid item xs={12} sm={6}>
         <Box
           component="form"
+          onSubmit={handleSubmit}
           sx={{
             display: "flex",
             flexDirection: "column",
@@ -70,12 +110,14 @@ const SignupPage = () => {
           >
             Cadastre-se
           </Typography>
-          <Typography sx={{
-            display: {
-              xs: "none",
-              sm: "block",
-            }
-          }}>
+          <Typography
+            sx={{
+              display: {
+                xs: "none",
+                sm: "block",
+              },
+            }}
+          >
             e conheça o que há de melhor no mundo da tecnologia e processamento
             jurídico
           </Typography>
@@ -86,12 +128,18 @@ const SignupPage = () => {
           >
             <Grid item xs={12} sm={6}>
               <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-amount" sx={{
+                <InputLabel
+                  htmlFor="nome"
+                  sx={{
                     fontWeight: 700,
-                  }}>
+                  }}
+                >
                   Nome
                 </InputLabel>
                 <OutlinedInput
+                  value={formData.nome}
+                  onChange={handleChange}
+                  name="nome"
                   id="input-nome"
                   label="Nome"
                   placeholder="Digite seu nome"
@@ -103,12 +151,18 @@ const SignupPage = () => {
             </Grid>
             <Grid item xs={12} sm={6}>
               <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-amount" sx={{
+                <InputLabel
+                  htmlFor="outlined-adornment-amount"
+                  sx={{
                     fontWeight: 700,
-                  }}>
+                  }}
+                >
                   Sobrenome
                 </InputLabel>
                 <OutlinedInput
+                  value={formData.sobrenome}
+                  onChange={handleChange}
+                  name="sobrenome"               
                   id="input-sobrenome"
                   label="Sobrenome"
                   placeholder="Digite seu sobrenome"
@@ -121,13 +175,19 @@ const SignupPage = () => {
             {/* Add more input fields here */}
             {/* For example: */}
             <Grid item xs={12} sm={6}>
-              <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="e-mail" sx={{
-                  fontWeight: 700,
-                }}>
+              <FormControl fullWidth variant="outlined" id="email">
+                <InputLabel
+                  htmlFor="e-mail"
+                  sx={{
+                    fontWeight: 700,
+                  }}
+                >
                   E-mail
-                </InputLabel >
+                </InputLabel>
                 <OutlinedInput
+                  value={formData.email}
+                  onChange={handleChange}
+                  name="email"
                   id="input-email"
                   label="E-mail"
                   placeholder="Digite o E-mail"
@@ -139,12 +199,18 @@ const SignupPage = () => {
             </Grid>
             <Grid item xs={12} sm={6}>
               <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="Phone"
+                <InputLabel
+                  htmlFor="Phone"
                   sx={{
                     fontWeight: 700,
                   }}
-                >Phone</InputLabel >
+                >
+                  Phone
+                </InputLabel>
                 <OutlinedInput
+                  value={formData.phone}
+                  onChange={handleChange}
+                  name="phone"
                   id="input-phone"
                   label="Phone"
                   placeholder="Digite o seu telefone"
@@ -156,12 +222,18 @@ const SignupPage = () => {
             </Grid>
             <Grid item xs={12} sm={6}>
               <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="password"
+                <InputLabel
+                  htmlFor="password"
                   sx={{
                     fontWeight: 700,
                   }}
-                >Password</InputLabel >
+                >
+                  Password
+                </InputLabel>
                 <OutlinedInput
+                  value={formData.password}
+                  onChange={handleChange}
+                  name="password"
                   id="input-password"
                   label="Password"
                   placeholder="Digite a sua senha"
@@ -173,12 +245,18 @@ const SignupPage = () => {
             </Grid>
             <Grid item xs={12} sm={6}>
               <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="confirm-pass"
+                <InputLabel
+                  htmlFor="confirm-pass"
                   sx={{
                     fontWeight: 700,
                   }}
-                >Confirme Password</InputLabel >
+                >
+                  Confirme Password
+                </InputLabel>
                 <OutlinedInput
+                  value={formData.confirmPass}
+                  onChange={handleChange}
+                  name="confirmPass"
                   id="input-confirm-pass"
                   label="Confirme sua senha"
                   placeholder="Confirme sua senha"
@@ -197,14 +275,37 @@ const SignupPage = () => {
               paddingTop: "3.5rem",
             }}
           >
-            <Button variant="contained" sx={{ 
-              color: "#fff", fontWeight: 700, 
-              fontSize: "1rem", padding: "1rem 0",
-              width: "50%"
-            }}>
+            <Button
+              variant="contained"
+              type="submit"
+              sx={{
+                color: "#fff",
+                fontWeight: 700,
+                fontSize: "1rem",
+                padding: "1rem 0",
+                width: "50%",
+              }}
+            >
               Cadastrar
             </Button>
+            
           </Box>
+          <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={() => setOpen(false)}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            
+          >
+            <Alert severity="error" sx={{ 
+              
+            }}>
+            <AlertTitle>Error</AlertTitle>
+              {
+                message
+              }
+            </Alert>
+          </Snackbar>
         </Box>
       </Grid>
     </Grid>
